df02baf5f240c0d89e406124569d29c54d7cabec
MrHalfs <148576550+MrHalfs@users.noreply.github.com>
2024-06-03 00:18:58 -0500

    Added modes

--- move.py
+++ move.py
@@ -3,17 +3,22 @@ from datetime import datetime, timedelta, time as time2
 import schedule
 import time
 import sys
-import threading
 
 
 class Screen:
-    def __init__(self, start: tuple, end: tuple):
+    def __init__(self, start: tuple, mode="office"):
         self.start = time2(start[0], start[1], start[2])
-        self.end = time2(end[0], end[1], end[2])
+        self.end = None
+        self.mode = mode
+        if self.mode == "office":
+            self.end = time2(6, 50, 0)
+        elif self.mode == "home":
+            self.end = time2(7, 55, 0)
 
     def send_alert(self):
         if not self.alert_status():
             x, y = pyautogui.position()
+            # company policies require exagerated movement. otherwise set the values to 1 and remove keyboard press
             pyautogui.moveTo(x - 100, y)
             pyautogui.moveTo(x + 100, y)
             pyautogui.moveTo(x, y)
@@ -29,8 +34,15 @@ class Screen:
 
 
 def main():
-    mover = Screen((0, 1, 0), (6, 50, 0))
-    print(f"Mouse will move between: {mover.start} AM - {mover.end} PM")
+    modes = ["office", "home"]
+    while True:
+        mode = input("Work location (home/office): ").lower().strip()
+        if mode not in modes:
+            print("Invalid option")
+        else:
+            mover = Screen((0, 1, 0), mode=mode)
+            break
+    print(f"Mouse will move between: {mover.start} AM - {mover.end} AM")
     schedule.every(9).minutes.do(mover.send_alert)
     while True:
         current = datetime.now().time()
